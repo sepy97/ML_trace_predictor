438f7730ac5062072f751c90a459006e7f6c54c9
sepy97 <sepy97@gmail.com>
2022-11-18 17:37:08 -0800

    added online learning perceptron

--- main.py
+++ main.py
@@ -2,44 +2,34 @@ import tracePredictor
 import perceptron
 import features
 import numpy as np
+#import onlineLearning as ol
+from skmultiflow.neural_networks import PerceptronMask
+from skmultiflow.data import DataStream 
 
 history_length = 5
 num_of_classes = 4
-#filePath = 'datafiles/ifthenelse/predictionAccuracy.csv'
-filePath = 'datafiles/libquantum/predictionAccuracy.csv'
+filePath = 'datafiles/ifthenelse/predictionAccuracy.csv'
+#filePath = 'datafiles/libquantum/predictionAccuracy.csv'
 
+### Different feature transformations
 print ("Binary classifier transform: ")
-#classfeatures, classlabels = features.generateTraceFeatures(filePath, history_length, num_of_classes)
-#print ("Features: ", classfeatures.shape, " Labels: ", classlabels.shape)
+classfeatures, classlabels = features.generateTraceFeatures(filePath, history_length, num_of_classes)
+print ("Features: ", classfeatures.shape, " Labels: ", classlabels.shape)
 
 print ("Bincode transform: ")
 #binfeatures, binlabels = features.generateBincodeTraceFeatures(filePath, history_length, num_of_classes)
 #print ("Features: ", binfeatures.shape, " Labels: ", binlabels.shape)
 
 print ("Without transform: ")
-wofeatures, wolabels = features.generateTraceFeaturesWithoutTransform(filePath, history_length, num_of_classes)
-print ("Features: ", wofeatures.shape, " Labels: ", wolabels.shape)
+#wofeatures, wolabels = features.generateTraceFeaturesWithoutTransform(filePath, history_length, num_of_classes)
+#print ("Features: ", wofeatures.shape, " Labels: ", wolabels.shape)
 
-#print ("class: ")
-#print (classfeatures[:5])
-#print ("bin: ")
-#print (binfeatures[:5])
-print ("wo: ")
-print (wofeatures[:5])
-#Shuffle the data
-if False:
-    indices = np.arange(classfeatures.shape[0])
-    np.random.shuffle(indices)
-    features = classfeatures[indices]
-    labels = classlabels[indices]
-    indices = np.arange(binfeatures.shape[0])
-    np.random.shuffle(indices)
-    features = binfeatures[indices]
-    labels = binlabels[indices]
-indices = np.arange(wofeatures.shape[0])
-np.random.shuffle(indices)
-features = wofeatures[indices]
-labels = wolabels[indices]
+
+
+### ONLINE LEARNING
+#Without shuffling
+features = classfeatures
+labels = classlabels
 
 # For debugging purposes, reducing the length of datasets
 features = features[:5000]
@@ -49,9 +39,41 @@ training_dataset = features[:int(len(features)*0.05)]
 training_labels = labels[:int(len(labels)*0.05)]
 test_dataset = features[int(len(features)*0.05):]
 test_labels = labels[int(len(labels)*0.05):]
-print ("Training dataset: ", training_dataset.shape, " Test dataset: ", test_dataset.shape)
+
 _, num_of_features = training_dataset.shape
-print ("Number of features: ", num_of_features)
+ol_perceptron_model = PerceptronMask()
+online_trace_predictor = tracePredictor.onlineTracePredictor(num_of_features, num_of_classes, training_dataset, training_labels, test_dataset, test_labels, ol_perceptron_model) 
+ol_error = online_trace_predictor.predict_and_update()
+print ("Online learning error: ", ol_error)
+
+
+
+### STATIC LEARNING
+#Shuffle the data
+
+if False:
+    indices = np.arange(binfeatures.shape[0])
+    np.random.shuffle(indices)
+    features = binfeatures[indices]
+    labels = binlabels[indices]
+
+    indices = np.arange(wofeatures.shape[0])
+    np.random.shuffle(indices)
+    features = wofeatures[indices]
+    labels = wolabels[indices]
+
+indices = np.arange(classfeatures.shape[0])
+np.random.shuffle(indices)
+features = classfeatures[indices]
+labels = classlabels[indices]
+
+# For debugging purposes, reducing the length of datasets
+features = features[:5000]
+labels = labels[:5000]
+training_dataset = features[:int(len(features)*0.15)]
+training_labels = labels[:int(len(labels)*0.15)]
+test_dataset = features[int(len(features)*0.15):]
+test_labels = labels[int(len(labels)*0.15):]
 
 
 perceptron_model = perceptron.perceptron(num_of_features, num_of_classes) # create the perceptron model
--- tracePredictor.py
+++ tracePredictor.py
@@ -1,6 +1,7 @@
 import perceptron
 import collections
 import numpy as np
+from skmultiflow.data import DataStream 
 class tracePredictor:
     def __init__(self, num_features, num_classes, model):
         self.num_features = num_features
@@ -37,3 +38,30 @@ class tracePredictor:
         dataset = np.hstack((np.zeros((M,1)), dataset))
         self.training_error = self.model.fit(dataset, labels)
         return self.training_error
+
+class onlineTracePredictor:
+    def __init__ (self, num_features, num_classes, training_dataset, training_labels, test_dataset, test_labels, model):
+        self.num_features = num_features
+        self.num_classes = num_classes
+        self.num_of_datapoints = test_dataset.shape[0]
+        self.model = model
+        self.target_values = list(np.arange(0, num_classes, 1))
+        self.datastream = DataStream(test_dataset, y=test_labels)
+        self.model.partial_fit(training_dataset, training_labels, self.target_values)
+        self.training_error = 0
+        self.test_error = 0
+
+    def predict_and_update(self):
+        total = 0
+        correct = 0
+        
+        while total < self.num_of_datapoints and self.datastream.has_more_samples():
+            X, y = self.datastream.next_sample()
+            trace_id = self.model.predict(X)
+            if y == trace_id:
+                correct += 1
+            total += 1
+            self.model.partial_fit(X, y, classes = self.target_values)
+
+        self.test_error = 1.0 - float(correct)/float(total)
+        return self.test_error
